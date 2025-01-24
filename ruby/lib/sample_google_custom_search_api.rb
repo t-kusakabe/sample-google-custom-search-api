dc7b76585f8cf831219a9000e77b54ab7bef4db0
t-kusakabe <ttmac0704@gmail.com>
2025-01-24 23:41:53 +0900

    検索結果をCSVに吐き出す

---
+++ ruby/lib/sample_google_custom_search_api.rb
@@ -0,0 +1,39 @@
+require 'google/apis/customsearch_v1'
+require 'csv'
+require 'pry'
+
+searcher = Google::Apis::CustomsearchV1::CustomSearchAPIService.new
+searcher.key = ENV.fetch('GOOGLE_API_KEY')
+
+user_profile = {
+  name: 'たけし',
+  interests: ['奈良', '学生'],
+  preferred_domains: ['yamatoji.nara-kankou.or.jp', 'ritsumei.ac.jp'],
+}
+
+search_query = '明日の天気'
+
+result = searcher.list_cses(
+  cx: ENV.fetch('CUSTOM_SEARCH_ENGINE_ID'),
+  q: "#{search_query} #{user_profile[:interests].join(' ')}",
+  lr: 'lang_ja',
+)
+result.search_information.formatted_total_results
+
+CSV.open('./data/result.csv', 'w') do |csv|
+  csv << [
+    'title',
+    'display_link',
+    'link',
+    'snippet',
+  ]
+
+  result.items.each do |item|
+    csv << [
+      item.title,
+      item.display_link,
+      item.link,
+      item.snippet,
+    ]
+  end
+end
